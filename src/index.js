e3034c4dcbf7e087dbdf180e67d0451a7c2bbd70
Tom Claudio <526473+gb96@users.noreply.github.com>
2019-11-18 01:08:56 +0800

    Create ref to control child Player component

--- src/index.js
+++ src/index.js
@@ -6,24 +6,26 @@ class Index extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
     }
+    this.player = React.createRef();
   }
 
+  play() {
+    this.player.current.play()
+  }
 
+  pause() {
+    this.player.current.pause()
+  }
 
   render() {
     return (
       <div style={{ position: 'relative' }} >
-        <Player />
+        <Player ref={this.player} />
       </div>
     )
   }
 }
-
-
-
-
 
 const mountPoint = document.getElementById('mount-point');
 ReactDOM.render(<Index />, mountPoint);
