832d66f10d74448822cb79736aff7d1e9dc2ad43
Tom Claudio <526473+gb96@users.noreply.github.com>
2019-11-18 14:54:30 +0800

    Hide buttons when url not set, create edit url button

--- src/Create/index.js
+++ src/Create/index.js
@@ -17,7 +17,7 @@ export default class Create extends React.Component {
   submitUrl(e) {
     e.preventDefault()
     console.log(e.target.elements.url.value)
-    this.setState({url:e.target.elements.url.value})
+    this.setState({ url: e.target.elements.url.value })
   }
 
   openModal() {
@@ -29,36 +29,45 @@ export default class Create extends React.Component {
   }
 
   closeModal() {
-    this.setState({openModal: false})
+    this.setState({ openModal: false })
+  }
+
+  clear() {
+    this.setState({ url: "" })
   }
 
   render() {
     return (
       <React.Fragment>
-        <form onSubmit={e => this.submitUrl(e)}>
-          <label htmlFor="urlfield">Please enter URL</label>
-          <input id="urlfield" type="url" name="url" />
-          <input type="submit" />
-        </form>
+        {this.state.url ?
+          <button onClick={() => this.clear()}>Change Video</button>
+          :
+          <form onSubmit={e => this.submitUrl(e)}>
+            <label htmlFor="urlfield">Please enter URL</label>
+            <input id="urlfield" type="url" name="url" />
+            <input type="submit" />
+          </form>
+        }
 
         <div style={{ position: 'relative', width: '640px', height: '390px', backgroundColor: 'black' }}>
-          {this.state.url ? 
+          {this.state.url ?
             <Player url={this.state.url} ref={this.player} />
-          :
-          ""
+            :
+            ""
           }
-          
-        </div>
 
-        <div>
-          <button onClick={() => this.openModal()}>Create new prompt</button>
         </div>
+        {this.state.url ?
+          <div>
+            <button onClick={() => this.openModal()}>Create new prompt</button>
+          </div>: "" }
 
-        {this.state.openModal ? 
+
+        {this.state.openModal ?
           <Modal toggleShow={() => this.closeModal()}>
             {this.state.currentTime}
-          </Modal> 
-          : "" }
+          </Modal>
+          : ""}
       </React.Fragment>
 
     )
