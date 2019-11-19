e2b791db8408a1fea7bb928c6e09b7faf0f1889b
Tom Claudio <526473+gb96@users.noreply.github.com>
2019-11-19 22:42:41 +0800

    Fix error in props; improve styling

--- src/Create/Questions.js
+++ src/Create/Questions.js
@@ -1,19 +1,22 @@
 import React from 'react';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faClock } from '@fortawesome/free-solid-svg-icons'
+
 export default props => {
   return (
     <div className="col-4" style={{ padding: '0 10px' }}>
-      {props.state.question ? 
-      props.state.questions.map(question => {
-        return (
-          <div style={style.question}>
-            <small>{`${question.type.toUpperCase()}`}</small>
-            <p>{`${question.question}`}</p>
-            <small>{`${question.elapsed}`}</small>
-          </div>
-        )
-      }) : <p>No questions listed </p> }
-    </div> 
+      {props.state ?
+        props.state.map(question => {
+          return (
+            <div style={style.question}>
+              <small>{`${question.type.toUpperCase()}`}</small>
+              <p>{`${question.question}`}</p>
+              <small><FontAwesomeIcon style={style.clock} icon={faClock} /> {`${question.elapsed}`}</small>
+            </div>
+          )
+        }) : <p>No questions listed </p>}
+    </div>
   )
 }
 
@@ -22,10 +25,14 @@ const style = {
 
   },
   question: {
-    padding: '15px', 
-    border: '1px solid gray', 
-    borderRadius: '4px', 
+    padding: '15px',
+    border: '1px solid gray',
+    borderRadius: '4px',
     marginBottom: '15px'
+  },
+  clock: {
+    fontSize: '12px'
   }
 
 }
+
