580e240d3e144a3f1b45d5578be6b5b81941619d
Tom Claudio <526473+gb96@users.noreply.github.com>
2019-11-19 11:05:03 +0800

    Import Jest & Enzyme; Create tests

---
+++ src/App.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import expect from 'expect';
+
+import { configure, mount } from 'enzyme';
+
+import App from './App';
+
+import Adapter from 'enzyme-adapter-react-16';
+import { Router, Link } from 'react-router-dom';
+
+configure({ adapter: new Adapter() });
+
+
+describe('Test AppComponent', () => {
+  it('Mounts', () => {
+    const wrapper = mount(<App />)
+    expect(true).toBeTruthy();
+  }),
+
+  it('Has correct Routes', () => {
+    const wrapper = mount(<App />);
+    let links = wrapper.find(Link);
+    let router = wrapper.find(Router)
+    expect(links.length).toEqual(3);
+    expect(router.length).toEqual(1);
+  })
+})
