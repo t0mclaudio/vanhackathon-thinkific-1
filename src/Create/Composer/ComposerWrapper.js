dd33950166c498eae4a55d8ba576ac580d1c7596
Tom Claudio <526473+gb96@users.noreply.github.com>
2019-11-19 19:09:19 +0800

    Add style to Composer wrapper; Add close button

--- src/Create/Composer/ComposerWrapper.js
+++ src/Create/Composer/ComposerWrapper.js
@@ -2,11 +2,36 @@ import React from 'react';
 
 export default props => {
   return (
-    <div>
+    <div style={style.overlay}>
+      <span
+        id="closePortalBtn"
+        style={style.closePortalBtn}
+        onClick={() => props.closeComposer()}>close [x]</span>
       {props.state.currentModule !== "A" ?
         <button onClick={() => props.updateModalModule("A")}>Back</button> : ""}
       <h3>Prompt will be positioned at {props.state.time.elapsed}</h3>
       {props.children}
     </div>
   )
+}
+
+const style = {
+  overlay: {
+    width: '640px',
+    height: '100%',
+    backgroundColor: '#222f3e',
+    zIndex: "99",
+    position: 'absolute',
+    borderRadius: '4px',
+  },
+  closePortalBtn: {
+    display: 'block',
+    width: '100%',
+    textAlign: 'right',
+    fontSize: '1em',
+    padding: '5px',
+    color: 'silver',
+    border: 'none',
+    cursor: 'pointer',
+  }
 }
