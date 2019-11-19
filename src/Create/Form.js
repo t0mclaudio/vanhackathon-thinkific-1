99085b78360321ac941b0c3ac1a5a43e8f7f553e
Tom Claudio <526473+gb96@users.noreply.github.com>
2019-11-19 13:12:18 +0800

    Create provisons for video title and description

--- src/Create/Form.js
+++ src/Create/Form.js
@@ -6,8 +6,15 @@ export default (props) => {
       <button onClick={() => props.clearURL()}>Change Video</button>
       :
       <form onSubmit={e => props.submitUrl(e)}>
+        {/* <label htmlFor="title">Please enter title</label>
+        <input id="title" type="text" name="title" required/>
+
+        <label htmlFor="description">Please enter description</label>
+        <textarea id="description" type="url" name="description" required/> */}
+
         <label htmlFor="urlfield">Please enter URL</label>
-        <input id="urlfield" type="url" name="url" />
+        <input id="urlfield" type="url" name="url" required/>
+
         <input type="submit" />
       </form>
   )
