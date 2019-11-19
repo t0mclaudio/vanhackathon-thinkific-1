13646155816b7e5c46f03f61548261d1a8aacbd6
Tom Claudio <526473+gb96@users.noreply.github.com>
2019-11-19 19:08:41 +0800

    Delete unused button; move button to player

--- src/Create/NewButton.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export default (props) => {
-  return (
-    props.state.info.url ?
-      <button onClick={() => props.openModal()}>Create new prompt</button> : ""
-  )
-}
