4f933b4843dc94a37bf96936fb51b4230d499a11
Tom Claudio <526473+gb96@users.noreply.github.com>
2019-11-19 20:49:40 +0800

    Clean up App

--- src/App.js
+++ src/App.js
@@ -7,26 +7,17 @@ import {
 } from 'react-router-dom';
 
 import Create from './Create';
-import Home from './Home';
-import Edit from './Edit';
 
 export default class App extends React.Component {
   render() {
     return (
       <div className="row">
         <Router>
-          <div className="col-lg-2">
-            <ul>
-              <li><Link to="/">Create</Link></li>
-            </ul>
-          </div>
-          <div className="col-lg-10">
             <Switch>
               <Route path="/">
                 <Create />
               </Route>
             </Switch>
-          </div>
         </Router>
       </div>
     )
