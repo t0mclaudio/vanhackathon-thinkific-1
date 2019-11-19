6bc67595c2fe6628f98b57eb933664cba86117a6
Tom Claudio <526473+gb96@users.noreply.github.com>
2019-11-19 14:32:49 +0800

    Add b4 layoouts to App

--- src/App.js
+++ src/App.js
@@ -13,24 +13,30 @@ import Edit from './Edit';
 export default class App extends React.Component {
   render() {
     return (
-      <Router>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/create">Create</Link></li>
-          <li><Link to="/edit">Edit</Link></li>
-        </ul>
-        <Switch>
-          <Route path="/create">
-            <Create />
-          </Route>
-          <Route path="/edit">
-            <Edit />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </Router>
+      <div className="row">
+        <Router>
+          <div className="col-lg-2">
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/create">Create</Link></li>
+              <li><Link to="/edit">Edit</Link></li>
+            </ul>
+          </div>
+          <div className="col-lg-10">
+            <Switch>
+              <Route path="/create">
+                <Create />
+              </Route>
+              <Route path="/edit">
+                <Edit />
+              </Route>
+              <Route path="/">
+                <Home />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
+      </div>
     )
   }
 }
