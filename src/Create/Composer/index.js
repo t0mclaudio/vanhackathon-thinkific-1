edf4c62211a27cc18120ee90a155ec784d4eb872
Tom Claudio <526473+gb96@users.noreply.github.com>
2019-11-19 22:07:17 +0800

    Fix time passed props

--- src/Create/Composer/index.js
+++ src/Create/Composer/index.js
@@ -8,9 +8,9 @@ export default (props) => {
     case "A":
       return <SelectQuestion updateModalModule={(id) => props.updateModalModule(id)} />
     case "B":
-      return <Identification time={props.state.time.currentSecond} handleSubmit={data => props.handleSubmit(data) } />
+      return <Identification time={props.state.time} handleSubmit={data => props.handleSubmit(data) } />
     case "C":
-      return <MultipleChoice time={props.state.time.currentSecond} handleSubmit={data => props.handleSubmit(data) } />
+      return <MultipleChoice time={props.state.time} handleSubmit={data => props.handleSubmit(data) } />
     default:
       return <SelectQuestion updateModalModule={(id) => props.updateModalModule(id)} />
   }  
