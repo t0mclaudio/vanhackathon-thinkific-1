3c73dce3c60e942823c92b5ef0e226ffa3163d02
Tom Claudio <526473+gb96@users.noreply.github.com>
2019-11-19 19:52:38 +0800

    Improve style of SelectQuestion

--- src/Create/Composer/SelectQuestion.js
+++ src/Create/Composer/SelectQuestion.js
@@ -1,13 +1,36 @@
 import React from 'react';
+import { whileStatement } from '@babel/types';
 
 export default props => {
   return (
-    <div>
-      <div>
-        <button onClick={() => props.updateModalModule("B")}>Fill in the blank</button>
-        <button onClick={() => props.updateModalModule("C")}>Multiple choice</button>
-        <button onClick={() => props.updateModalModule("D")}>True or False</button>
+    <div style={style.wrapper}>
+      <p>What type of question do you want to create?</p>
+      <div style={style.choices}>
+        <div style={style.choice} onClick={() => props.updateModalModule("B")}>Fill in the blank</div>
+        <div style={style.choice} onClick={() => props.updateModalModule("C")}>Multiple choice</div>
       </div>
     </div>
   )
+}
+
+const style= {
+  wrapper : {
+    padding: '15px',
+    color: 'white'
+  },
+  choices: {
+    display: 'grid',
+    gridTemplateColumns: 'auto',
+    gridGap: '15px'
+  },
+
+  choice: {
+    backgroundColor: 'white',
+    color: '#222f3e',
+    borderRadius: '4px',
+    height: '48px',
+    padding: '10px',
+    fontSize: '16px',
+    cursor: 'pointer'
+  }
 }
