1694fb7295093bbb20fa6fd0e0cddacb97babb15
Tom Claudio <526473+gb96@users.noreply.github.com>
2019-11-20 07:50:41 +0800

    Create route to View, data to state from Create

--- src/App.js
+++ src/App.js
@@ -7,15 +7,31 @@ import {
 } from 'react-router-dom';
 
 import Create from './Create';
+import View from './View';
+
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: {}
+    }
+  }
+
+  handleViewVideo(data) {
+    this.setState({data:data})
+  }
+
   render() {
     return (
       <div className="row">
         <Router>
             <Switch>
-              <Route path="/">
-                <Create />
+              <Route exact path="/">
+                <Create handleViewVideo={data => this.handleViewVideo(data)} />
+              </Route>
+              <Route exact path="/view">
+                <View data={this.state.data} />
               </Route>
             </Switch>
         </Router>
