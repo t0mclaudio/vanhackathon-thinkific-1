0dbc74d4ed52e42a30c3aaaaf87f616c047de693
Tom Claudio <526473+gb96@users.noreply.github.com>
2019-11-20 14:11:10 +0800

    Create Question types Modules

---
+++ src/View/Questions/Choices.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+export default props => {
+  return (
+    <form onSubmit={(e) => props.submitAnswer(e)}>
+      {props.state.wrongAnswer ?
+        <div>
+          <small id="passwordHelp" className="text-danger">
+            That is the wrong answer. Try again
+        </small>
+        </div>
+        : ""
+      }
+
+      {props.state.question.choices.map((choice, index) => {
+        let isSelected = choice === props.state.answer ? style.selected : style.choice
+        return (
+          <p style={isSelected} key={index} onClick={() => props.selectAnswer(choice)}>{choice}</p> 
+        )
+      })}
+
+
+      <input type="submit" className="btn btn-warning mt-4" />
+    </form>
+  )
+}
+
+const style = {
+  correct: "form-control",
+  wrong: "form-control is-invalid",
+  choice: {
+    padding: '12px',
+    margin: '10px',
+    backgroundColor: 'white',
+    borderRadius: '4px',
+    color: '#222f3e',
+    fontSize: '15px'
+  },
+  selected: {
+    padding: '12px',
+    margin: '10px',
+    backgroundColor: '#10ac84',
+    borderRadius: '4px',
+    color: '#222f3e',
+    fontSize: '15px'    
+  }
+}
