618c4b3db2a656314db53497aa7802afb05b9862
Tom Claudio <526473+gb96@users.noreply.github.com>
2019-12-14 06:57:02 +0800

    Use Hooks on Form

--- src/Create/Form.js
+++ src/Create/Form.js
@@ -1,46 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default class Form extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      url: "",
-      title: "",
-      description: ""
-    }
+export default (props) => {
+  const initialState = {
+    url: "",
+    title: "",
+    description: "",
   }
+  const [info, setInfo] = useState(initialState);
 
-  handleChange(e) {
-    this.setState({
-      [e.target.name]: e.target.value
-    })
-  }
+  const handleChange = (e) => setInfo({...info, [e.target.name]: e.target.value });
 
-  handleInfoSubmit(e) {
+  const handleInfoSubmit = (e) => {
     e.preventDefault()
-    this.props.handleInfoSubmit(this.state)
-  }
+    props.handleInfoSubmit(info)
+  };
 
-  render() {
-    return (
-      <form onSubmit={e => this.handleInfoSubmit(e)} style={{padding: '35px'}}>
-        <div className="form-group">
-          <label htmlFor="title" style={style.label} >Please enter title</label>
-          <input id="title" className="form-control" type="text" name="title" onChange={e => this.handleChange(e)} value={this.state.title} />
-        </div>
-        <div className="form-group">
-          <label htmlFor="description" style={style.label}>Please enter description</label>
-          <textarea id="description" className="form-control" type="url" name="description" onChange={e => this.handleChange(e)} value={this.state.description} />
-        </div>
-        <div className="form-group">
-          <label htmlFor="urlfield" style={style.label}>Please enter Video URL</label>
-          <input id="urlfield" className="form-control" type="url" name="url" onChange={e => this.handleChange(e)} value={this.state.url} />
-        </div>
+  return (
+    <form onSubmit={e => handleInfoSubmit(e)} style={{ padding: '35px' }}>
+      <div className="form-group">
+        <label htmlFor="title" style={style.label} >Please enter title</label>
+        <input id="title" className="form-control" type="text" name="title" onChange={e => handleChange(e)} value={info.title} />
+      </div>
+      <div className="form-group">
+        <label htmlFor="description" style={style.label}>Please enter description</label>
+        <textarea id="description" className="form-control" type="url" name="description" onChange={e => handleChange(e)} value={info.description} />
+      </div>
+      <div className="form-group">
+        <label htmlFor="urlfield" style={style.label}>Please enter Video URL</label>
+        <input id="urlfield" className="form-control" type="url" name="url" onChange={e => handleChange(e)} value={info.url} />
+      </div>
 
-        <input type="submit" className="btn btn-warning" />
-      </form>
-    )
-  }
+      <input type="submit" className="btn btn-warning" />
+    </form>
+  )
 }
 
 const style = {
