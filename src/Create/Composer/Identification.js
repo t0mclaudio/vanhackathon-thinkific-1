540f2325b74ebd5ecff0b8625e05ffcb59c90083
Tom Claudio <526473+gb96@users.noreply.github.com>
2019-12-14 08:49:46 +0800

    Refactor & use Hooks, add proptypes

--- src/Create/Composer/Identification.js
+++ src/Create/Composer/Identification.js
@@ -1,41 +1,48 @@
-import React from 'react';
+/* eslint-disable react/destructuring-assignment */
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 
-export default class FillTheBlank extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      question: "",
-      answer: "",
-      time: props.time.currentSecond,
-      elapsed: props.time.elapsed,
-      type: "identification"
-    }
-  }
-  handleChange(event) {
-    console.log(event.target.name)
-    this.setState({ [event.target.name]: event.target.value });
-    //console.log(this.state)
-  }
+const Identification = (props) => {
+  const initialState = {
+    question: '',
+    answer: '',
+    time: props.time.currentSecond,
+    elapsed: props.time.elapsed,
+    type: 'identification',
+  };
 
-  handleSubmit(e) {
-    e.preventDefault()
-    //console.log(this.state)
-    this.props.handleSubmit(this.state)
-  }
+  const [state, setState] = useState(initialState);
 
-  render() {
-    return (
-      <form onSubmit={e => this.handleSubmit(e)}>
-        <div className="form-group">
-          <label htmlFor="question">Question</label>
-          <textarea id="question" name="question" value={this.state.question} onChange={e => this.handleChange(e)} className="form-control" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="answer">Answer</label>
-          <input id="answer" type="text" name="answer" value={this.state.answer} onChange={e => this.handleChange(e)} className="form-control" />
-        </div>
-        <input type="submit" value="Submit" className="btn btn-warning" />
-      </form>
-    )
-  }
-}
+  const handleChange = (event) => {
+    setState({ ...state, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    props.handleSubmit(state);
+  };
+
+  return (
+    <form onSubmit={e => handleSubmit(e)}>
+      <div className="form-group">
+        <label htmlFor="question">Question</label>
+        <textarea id="question" name="question" value={state.question} onChange={(e) => handleChange(e)} className="form-control" />
+      </div>
+      <div className="form-group">
+        <label htmlFor="answer">Answer</label>
+        <input id="answer" type="text" name="answer" value={state.answer} onChange={(e) => handleChange(e)} className="form-control" />
+      </div>
+      <input type="submit" value="Submit" className="btn btn-warning" />
+    </form>
+  );
+};
+
+Identification.propTypes = {
+  time: PropTypes.shape({
+    currentSecond: PropTypes.number.isRequired,
+    elapsed: PropTypes.string.isRequired,
+  }).isRequired,
+  handleSubmit: PropTypes.func.isRequired,
+};
+
+export default Identification;
