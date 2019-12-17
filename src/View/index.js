693c26f39d4eda6e734fdd57f94cea6c68945974
Tom Claudio <526473+gb96@users.noreply.github.com>
2019-12-18 02:39:59 +0800

    Refactor

--- src/View/index.js
+++ src/View/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-wrap-multilines */
 import React from 'react';
 import Player from '../Components/Player';
 import Canvas from '../Create/Canvas';
@@ -11,77 +12,80 @@ export default class View extends React.Component {
       prompted: false,
       question: null,
       getEmail: {
-        question: "To continue, type in your email address?",
+        question: 'To continue, type in your email address?',
         passThrough: true,
         type: 'get email',
-        answer: ""
+        answer: '',
       },
-      data: this.props.data,
-      embedCode: ""
+      data: props.data,
+      embedCode: '',
     }
     this.player = React.createRef();
   }
 
   componentWillReceiveProps(props) {
-    this.setState({ embedCode: props.embedCode })
+    this.setState({ embedCode: props.embedCode });
   }
 
   openPrompt() {
     this.player.current.pause();
     this.setState({
       prompted: true,
-      time: this.player.current.reportTime()
-    })
+      time: this.player.current.reportTime(),
+    });
   }
 
   closePrompt() {
-    this.setState({ prompted: false, activeInModal: 'A' })
+    this.setState({ prompted: false, activeInModal: 'A' });
   }
 
   continue() {
-    this.player.current.play()
-    this.setState({ prompted: false })
+    this.player.current.play();
+    this.setState({ prompted: false });
   }
 
   reportElapsedSeconds(elapsed) {
     if (elapsed === 1) {
       this.player.current.pause();
       this.player.current.seekTo(elapsed);
-      this.setState({ prompted: true, question: this.state.getEmail })
+      this.setState({ prompted: true, question: this.state.getEmail });
     }
     if (this.state.data.questions.find(q => q.time === elapsed)) {
-      this.player.current.pause()
-      this.player.current.seekTo(elapsed)
-      let question = this.state.data.questions.find(q => q.time === elapsed)
-      this.setState({ prompted: true, question: question })
+      this.player.current.pause();
+      this.player.current.seekTo(elapsed);
+      const question = this.state.data.questions.find(q => q.time === elapsed);
+      this.setState({ prompted: true, question });
     }
   }
 
   render() {
+    const { info } = this.data;
+    const { prompted, embedCode, question } = this.state;
     return (
-      <React.Fragment>
-        {this.state.data.info ?
-          <Canvas>
-            {this.state.prompted ?
-              <QuestionWrapper state={this.state} closePrompt={() => this.closePrompt()}>
-                <Questions q={this.state.question} continue={() => this.continue()} />
-              </QuestionWrapper>
-              : ""}
-            <Player
-              info={this.state.data.info}
-              ref={this.player}
-              reportElapsedTime={(e) => this.reportElapsedSeconds(e)}
-              handleInsertClick={null}
-            />
-          </Canvas>
+      <>
+        {info
+          ? (
+            <Canvas>
+              { prompted
+                && (
+                <QuestionWrapper state={this.state} closePrompt={() => this.closePrompt()}>
+                  <Questions q={question} continue={() => this.continue()} />
+                </QuestionWrapper>)}
+              <Player
+                info={info}
+                ref={this.player}
+                reportElapsedTime={(e) => this.reportElapsedSeconds(e)}
+                handleInsertClick={null}
+              />
+            </Canvas>)
           : <h1 style={{ textAlign: 'center' }}>No title found</h1>}
-        {this.state.embedCode ? 
-          <div style={{width: '640px', marginTop: "15px"}}>
-            <h5>Embed Code</h5>    
-            <textarea readOnly value={this.state.embedCode} className="form-control" style={{height: '237px', fontSize: '13px'}} />
-          </div>
-        : null}
-      </React.Fragment>
-    )
+        { embedCode
+          && (
+          <div style={{ width: '640px', marginTop: '15px' }}>
+            <h5>Embed Code</h5>
+            <textarea readOnly value={embedCode} className="form-control" style={{ height: '237px', fontSize: '13px' }} />
+          </div>)}
+      </>
+    );
   }
 }
