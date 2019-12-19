d2187b6a38fb239e179b57ec7d14f8cd216c8218
Tom Claudio <526473+gb96@users.noreply.github.com>
2019-12-19 16:01:33 +0800

    Refactor Form.js, use context api

--- src/Create/Form.js
+++ src/Create/Form.js
@@ -1,42 +1,33 @@
-import React, { useState } from 'react';
-
-export default (props) => {
-  const initialState = {
-    url: "",
-    title: "",
-    description: "",
-  }
-  const [info, setInfo] = useState(initialState);
-
-  const handleChange = (e) => setInfo({...info, [e.target.name]: e.target.value });
-
-  const handleInfoSubmit = (e) => {
-    e.preventDefault()
-    props.handleInfoSubmit(info)
-  };
-
-  return (
-    <form onSubmit={e => handleInfoSubmit(e)} style={{ padding: '35px' }}>
-      <div className="form-group">
-        <label htmlFor="title" style={style.label} >Please enter title</label>
-        <input id="title" className="form-control" type="text" name="title" onChange={e => handleChange(e)} value={info.title} />
-      </div>
-      <div className="form-group">
-        <label htmlFor="description" style={style.label}>Please enter description</label>
-        <textarea id="description" className="form-control" type="url" name="description" onChange={e => handleChange(e)} value={info.description} />
-      </div>
-      <div className="form-group">
-        <label htmlFor="urlfield" style={style.label}>Please enter Video URL</label>
-        <input id="urlfield" className="form-control" type="url" name="url" onChange={e => handleChange(e)} value={info.url} />
-      </div>
-
-      <input type="submit" className="btn btn-warning" />
-    </form>
-  )
-}
+import React from 'react';
+import { Consumer } from '../Context';
 
 const style = {
   label: {
-    color: 'white'
-  }
-}
+    color: 'white',
+  },
+};
+
+export default () => (
+  <Consumer>
+    {({
+      url, title, description, actions,
+    }) => (
+      <form onSubmit={(e) => actions.handleInfoSubmit(e)} style={{ padding: '35px' }}>
+        <div className="form-group">
+          <label htmlFor="title" style={style.label}>Please enter title</label>
+          <input id="title" className="form-control" type="text" name="title" onChange={(e) => actions.handleChange(e)} value={title} />
+        </div>
+        <div className="form-group">
+          <label htmlFor="description" style={style.label}>Please enter description</label>
+          <textarea id="description" className="form-control" type="url" name="description" onChange={(e) => actions.handleChange(e)} value={description} />
+        </div>
+        <div className="form-group">
+          <label htmlFor="urlfield" style={style.label}>Please enter Video URL</label>
+          <input id="urlfield" className="form-control" type="url" name="url" onChange={(e) => actions.handleChange(e)} value={url} />
+        </div>
+
+        <input type="submit" className="btn btn-warning" />
+      </form>
+    )}
+  </Consumer>
+);
